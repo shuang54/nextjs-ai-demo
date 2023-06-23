807d226b5a6177a64ac42a683f79bed7190864f6
shuang <shuang.lixx@qq.com>
2023-06-23 22:52:44 +0800

    fix :UI

--- app/page.tsx
+++ app/page.tsx
@@ -14,17 +14,19 @@ export default function Chat() {
         </div>
       ))}
 
-      <form onSubmit={handleSubmit}>
-        <label>
-          Say something...
-          <input
-            className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2"
-            value={input}
-            onChange={handleInputChange}
-          />
-        </label>
-        <button type="submit">Send</button>
-      </form>
+      <div className="mt-auto fixed bottom-0 mb-8">
+        <form onSubmit={handleSubmit} className='flex justify-center items-center gap-3 ]'>
+          <label className=' relative select-none'>
+            <input
+              placeholder='Say something...'
+              className=" w-full max-w-md border border-gray-300 rounded  shadow-xl p-2"
+              value={input}
+              onChange={handleInputChange}
+            />
+          </label>
+          <button type="submit" className=' select-none'>Send</button>
+        </form>
+      </div>
     </div>
   )
 }
